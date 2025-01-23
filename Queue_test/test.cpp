355e2d9243f756cdbb3bed1d6af4438e4b91230d
kmag-10129 <kjoshua.magtibay@gmail.com>
2025-01-23 15:41:27 +1300

    Added a more significant test: - uses a new constructor for an existing queue - tests all existing functionality in one go

--- Queue_test/test.cpp
+++ Queue_test/test.cpp
@@ -2,7 +2,6 @@
 #include "../Queue/Queue.h"
 
 MyQueue test_queue; // used for testing with empty queue
-MyQueue env_queue_1({ 2,3,0,4 }); // used for testing with a prefilled queue
 namespace QueueTest {
 	// first test is to check if queue IS empty.
 	TEST(QueueEmpty, t_QueueIsEmpty) {
@@ -39,5 +38,25 @@ namespace QueueTest {
 		EXPECT_EQ(2, test_queue.Queue_at_front()); // is the element at the front a 2?
 	}
 
+	// more significant test using the prefilled queue:
+	// - check that the queue is NOT empty at the beginning
+	// - append 1, 5, 9, 12
+	// - dequeue five times
+	// - check the size (should be 3)
+	// - check the first item in the queue (should be five)
+	TEST(EnvQueueTest, t_EnvTest1) {
+		MyQueue env_queue_1({ 2,3,0,4 }); // used for testing with a prefilled queue
+		EXPECT_FALSE(env_queue_1.Queue_empty()); // this should be false as on initialization queue is {2, 3, 0, 4}
+		EXPECT_EQ(SUCCESS, env_queue_1.Queue_add(1)); // add a 1
+		EXPECT_EQ(SUCCESS, env_queue_1.Queue_add(5)); // add a 5
+		EXPECT_EQ(SUCCESS, env_queue_1.Queue_add(9)); // add a 9
+		EXPECT_EQ(SUCCESS, env_queue_1.Queue_add(12)); // add a 12
+		// dequeue five times
+		for (int i = 0; i < 5; i++) {
+			EXPECT_EQ(SUCCESS, env_queue_1.Queue_remove());
+		}
+		EXPECT_EQ(3, env_queue_1.Queue_get_size()); // size should be 3
+		EXPECT_EQ(5, env_queue_1.Queue_at_front()); // is the element at the front a 5?
+	}
 }
 
